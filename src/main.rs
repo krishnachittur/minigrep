662437cabe09408f23a2b4cfc8c11f898187edf2
Krishna Chittur <kchittur@utexas.edu>
2018-11-10 11:03:42 -0600

    command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        println!("Usage: <program> <query> <filename>");
+        return;
+    }
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for {} in file {}", query, filename);
 }
